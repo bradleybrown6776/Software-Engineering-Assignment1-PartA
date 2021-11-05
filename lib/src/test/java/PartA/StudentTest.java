f20f05d98e2d0060f461a14284a11fafe724e2af
bradleybrown6776 <61282130+bradleybrown6776@users.noreply.github.com>
2021-11-05 16:04:42 +0000

    Update StudentTest.java

--- lib/src/test/java/PartA/StudentTest.java
+++ lib/src/test/java/PartA/StudentTest.java
@@ -10,14 +10,14 @@ public class StudentTest {
 
     @Before public void init() {
         // Reset student test object before each test.
-        student = new Student("Eoin McArdle", 21, "05/12/1999", 198181929);
+        student = new Student("Bradley Brown", 21, 198181929);
     }
 
     @Test public void getUserNameReturnsCorrectUsername() {
-        assertEquals("Eoin McArdle-05/12/1999", student.getUsername());
+        assertEquals("Brad12", student.getUsername());
     }
 
-    @Test public void modulesCanBeAddedToStudent() {
+    @Test public void canAddModulesToStudent() {
         Module module = new Module("Software Engineering","CT417");
         ArrayList<Module> modules = new ArrayList<Module>();
         modules.add(module);
@@ -25,24 +25,4 @@ public class StudentTest {
         assertEquals(modules, student.getModules());
     }
 
-    @Test public void coursesCanBeAddedToStudent() {
-        Course course = new Course("CS & IT",DateTime.now(), DateTime.now());
-        ArrayList<Course> courses = new ArrayList<Course>();
-        courses.add(course);
-        student.setCourses(courses);
-        assertEquals(courses, student.getCourses());
-    }
-
-    @Test public void toStringOutputsCorrectRepresentation() {
-        Module module = new Module("Software Engineering","CT417");
-        ArrayList<Module> modules = new ArrayList<Module>();
-        modules.add(module);
-        student.setModules(modules);
-        Course course = new Course("CS & IT",DateTime.now(), DateTime.now());
-        ArrayList<Course> courses = new ArrayList<Course>();
-        courses.add(course);
-        student.setCourses(courses);
-        String out = student.toString();
-        assertEquals("Eoin McArdle-05/12/1999\nModules:\n\tSoftware Engineering\nCourses:\n\tCS & IT\n", out);
-    }
 }
